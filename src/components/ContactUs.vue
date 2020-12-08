013a2f97cba76b03c6353c80dc2ac53d057f7f27
Boban <boban0092@gmail.com>
2020-12-08 19:51:08 +0100

    [TASK] #3 Contact Us Page

--- src/components/ContactUs.vue
+++ src/components/ContactUs.vue
@@ -1,34 +1,58 @@
 <template>
-    <div>
-        <h1>Contact Us</h1>
+    <!-- CONTACT FORM BEGINNING -->
+    <div class="contact__form-wrapper">
+        <div class="contact__form-wrapper-inner">
+            <form id="contact__form"
+                  type="post">
+                <h2>Contact Us</h2>
+                <div class="contact__form-container">
+                    <label for="email-address">Enter your email:</label>
+                    <input placeholder="Email address"
+                           autocomplete="off"
+                           type="text"
+                           name="email"
+                           id="email-address"/>
+                </div>
+
+                <div class="contact__form-container">
+                    <label for="message">Message:</label>
+                    <textarea placeholder="Send us message"
+                              autocomplete="off"
+                              type="text"
+                              name="message"
+                              id="message"/>
+                </div>
+                <div class="contact__form-submit-container">
+                    <input id="submit-contact-form"
+                           class="contact__form-button"
+                           type="submit"
+                           name="submit-contact-form"/>
+                </div>
+            </form>
+        </div>
     </div>
+    <!-- CONTACT FORM END -->
 </template>
 
 
 <script>
     export default {
         components: {},
-        data:function(){
-            return {
-
-            }
+        data: function () {
+            return {}
         },
 
-        methods:{
-
-        },
+        methods: {},
 
         created() {
 
         },
 
-        computed:{
-
-        }
+        computed: {}
     }
 </script>
 
 
 <style lang="scss" scoped>
-
+    @import "../assets/stylesheets/sections/contact-form";
 </style>
