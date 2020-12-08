f8eafc4dc0124b398181985f31ab466aac1abd15
Boban <boban0092@gmail.com>
2020-12-08 18:53:50 +0100

    [TASK] #2: Home Page

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <div class="content-main">
         <Header></Header>
         <keep-alive>
             <router-view></router-view>
@@ -19,5 +19,28 @@
 </script>
 
 <style lang="scss">
+    body {
+        margin: 0 !important;
+        position: relative;
+    }
 
+    /* width */
+    ::-webkit-scrollbar {
+        width: 5px;
+    }
+
+    /* Track */
+    ::-webkit-scrollbar-track {
+        background: #f1f1f1;
+    }
+
+    /* Handle */
+    ::-webkit-scrollbar-thumb {
+        background: $color-background-icon;
+    }
+
+    /* Handle on hover */
+    ::-webkit-scrollbar-thumb:hover {
+        background: #555;
+    }
 </style>
--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,19 +1,240 @@
 <template>
     <div>
-        <h1>Home Page</h1>
+        <!-- SECTION-ABOUT BEGINNING -->
+        <section class="sectionAbout">
+            <div class="sectionAbout__container">
+                <div class="sectionAbout__container-inner">
+                    <div class="sectionAbout__headline-wrap">
+                        <h1 class="sectionAbout__headline">Connect to the world</h1>
+                    </div>
+                    <div class="sectionAbout__content">
+                        <div class="sectionAbout__text">
+                            <div class="sectionAbout__text-container">
+                                <h3>Our world deserves more you</h3>
+                                <p>
+                                    Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
+                                    tempor
+                                    invidunt ut nlabore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et
+                                    accusam et justo duo
+                                    dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem
+                                    ipsum
+                                    dolor sit amet. Lorem
+                                    ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor
+                                    invidunt.
+                                </p>
+                            </div>
+                            <div class="sectionAbout__text-container">
+                                <h3>Whatever your style, see it your way</h3>
+                                <p>
+                                    Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
+                                    tempor
+                                    invidunt ut nlabore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et
+                                    accusam et justo duo
+                                    dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem
+                                    ipsum
+                                    dolor sit amet. Lorem
+                                    ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor
+                                    invidunt.
+                                </p>
+                            </div>
+                        </div>
+                        <div class="sectionAbout__image">
+                            <img src="/src/assets/icons/images/img-connect.jpg">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- SECTION-ABOUT END -->
+
+        <!-- SECTION-FEATURES BEGINNING -->
+        <section class="sectionFeatures">
+            <div class="sectionFeatures__container">
+                <div class="sectionFeatures__container-inner">
+                    <div class="sectionFeatures__headline-wrap">
+                        <h2 class="sectionFeatures__headline">why adventure travel?</h2>
+                    </div>
+                    <div class="sectionFeatures__item-wrap">
+                        <div class="sectionFeatures__item">
+                            <div class="sectionFeatures__item-icon sectionFeatures__item-icon--airplane"></div>
+                            <h4 class="sectionFeatures__item-headline">
+                                explore the world
+                            </h4>
+
+                            <p class="sectionFeatures__item-text">
+                                Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
+                                sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat.
+                            </p>
+
+                        </div>
+                        <div class="sectionFeatures__item">
+                            <div class="sectionFeatures__item-icon sectionFeatures__item-icon--earth"></div>
+                            <h4 class="sectionFeatures__item-headline">
+                                meet nature
+                            </h4>
+                            <p class="sectionFeatures__item-text">
+                                Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
+                                sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat.
+                            </p>
+                        </div>
+                        <div class="sectionFeatures__item">
+                            <div class="sectionFeatures__item-icon sectionFeatures__item-icon--compass"></div>
+                            <h4 class="sectionFeatures__item-headline">
+                                know your way
+                            </h4>
+                            <p class="sectionFeatures__item-text">
+                                Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
+                                sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat.
+                            </p>
+                        </div>
+                        <div class="sectionFeatures__item">
+                            <div class="sectionFeatures__item-icon sectionFeatures__item-icon--heart"></div>
+                            <h4 class="sectionFeatures__item-headline">
+                                live a healthier life
+                            </h4>
+                            <p class="sectionFeatures__item-text">
+                                Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
+                                sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- SECTION-FEATURES END -->
+
+        <!-- SECTION-TOURS BEGINNING -->
+        <section class="sectionTours">
+            <div class="sectionTours__container">
+                <div class="sectionTours__container-inner">
+                    <div class="sectionTours__headline-wrap">
+                        <h2 class="sectionTours__headline">most popular tours</h2>
+                    </div>
+                    <div class="sectionTours__item-wrap">
+                        <div class="sectionTours__item">
+                            <a class="sectionTours__item--link" href="">
+                                <div class="sectionTours__item-image">
+                                    <img src="/src/assets/icons/images/img-sea.jpg" width="300" height="200">
+                                </div>
+                                <div class="sectionTours__item-content">
+                                    <h3 class="sectionTours__item-headline">
+                                        the sea explorer
+                                    </h3>
+                                    <ul class="sectionTours__item-list">
+                                        <li>3 day tours</li>
+                                        <li>Up to 30 people</li>
+                                        <li>2 tour guides</li>
+                                        <li>Sleep in cozy hotels</li>
+                                        <li>Difficulty: easy</li>
+                                    </ul>
+                                </div>
+                            </a>
+                        </div>
+                        <div class="sectionTours__item">
+                            <a class="sectionTours__item--link" href="">
+                                <div class="sectionTours__item-image">
+                                    <img src="/src/assets/icons/images/img-snow.jpg" width="300" height="200">
+                                </div>
+                                <div class="sectionTours__item-content">
+                                    <h3 class="sectionTours__item-headline">
+                                        the snow adventurer
+                                    </h3>
+                                    <ul class="sectionTours__item-list">
+                                        <li>5 day tours</li>
+                                        <li>Up to 15 people</li>
+                                        <li>3 tour guides</li>
+                                        <li>Sleep in provided tents</li>
+                                        <li>Difficulty: hard</li>
+                                    </ul>
+                                </div>
+                            </a>
+                        </div>
+                        <div class="sectionTours__item">
+                            <a class="sectionTours__item--link" href="">
+                                <div class="sectionTours__item-image">
+                                    <img src="/src/assets/icons/images/img-forest.jpg" width="300" height="200">
+                                </div>
+                                <div class="sectionTours__item-content">
+                                    <h3 class="sectionTours__item-headline">
+                                        the forest hiker
+                                    </h3>
+                                    <ul class="sectionTours__item-list">
+                                        <li>7 day tours</li>
+                                        <li>Up to 40 people</li>
+                                        <li>6 tour guides</li>
+                                        <li>Sleep in provided tents</li>
+                                        <li>Difficulty: medium</li>
+                                    </ul>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="sectionTours__button">
+                        <a class="sectionTours__button--link">
+                            discover all tours
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- SECTION-TOURS END -->
+
+
+        <!-- FOOTER SECTION BEGINNING -->
+        <footer class="footer">
+            <div class="footer__container--top">
+                <div class="footer__container-inner--top">
+                    <div class="footer__nav">
+                        <ul class="footer__nav-list">
+                            <li class="footer__nav-item">
+                                <a class="footer__nav-item--link">Destinations</a>
+                            </li>
+                            <li class="footer__nav-item">
+                                <a class="footer__nav-item--link">Travel Styles</a>
+                            </li>
+                            <li class="footer__nav-item">
+                                <a class="footer__nav-item--link">Travel Deals</a>
+                            </li>
+                            <li class="footer__nav-item">
+                                <a class="footer__nav-item--link">Gear</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="footer__icons">
+                        <a href="/" class="footer__twitter"></a>
+                        <a href="/" class="footer__instagram"></a>
+                        <a href="/" class="footer__facebook"></a>
+                    </div>
+                </div>
+            </div>
+            <div class="footer__container--bottom">
+                <div class="footer__container-inner--bottom">
+                    <div class="footer__logo-container">
+                        <a class="footer__logo" href="/index.html">
+                            always outdors
+                        </a>
+                    </div>
+                    <div class="footer__copyrights">
+                        © 2019, Always Outside
+                    </div>
+                </div>
+            </div>
+        </footer>
+        <!-- FOOTER SECTION END -->
     </div>
 </template>
 
 <script>
     export default {
-        data:function(){
-            return {
-
-            }
+        data: function () {
+            return {}
         }
     }
 </script>
 
 <style lang="scss" scoped>
-
+    @import "../assets/stylesheets/sections/section-about";
+    @import "../assets/stylesheets/sections/section-features";
+    @import "../assets/stylesheets/sections/section-tours";
+    @import "../assets/stylesheets/sections/footer";
 </style>
